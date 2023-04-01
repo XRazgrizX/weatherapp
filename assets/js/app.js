7a5d01e06f645adadcd77de01e56363161df5159
Razgriz <enzerux@gmail.com>
2023-04-01 16:18:15 +0200

    v1

---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+let weatherForm = document.querySelector('.weather__form')
+let inputCity = document.querySelector('.weather__city')
+let apiUrl = 'https://api.weatherapi.com/v1/current.json?key=82089a454a5a462da3f131227230104&aqi=yes&q='
+let apiDataContainer = document.querySelector('.weather__data')
+
+weatherForm.addEventListener('submit', (event) => {
+    let city = inputCity.value
+    let fullApiUrl = apiUrl + city
+
+    fetch(fullApiUrl).then(response => response.json()).then((dataFromApi) => {
+        // console.log(dataFromApi.current.temp_c)
+        let view = ``
+        // view += `W ${dataFromApi.location.name} jest dzisiaj ${dataFromApi.current.temp_c} stopni Celsjusza.`
+
+        view += `<div class="weather__info">`
+
+        view += `<div class="weather__icon">
+        
+        <img src="${dataFromApi.current.condition.icon}" alt="${dataFromApi.current.condition.text}">
+        
+        </div>`
+
+        view += `<div class="weather__temp">
+        
+        ${dataFromApi.current.temp_c}<span>&deg;C</span>
+
+        </div>`
+
+        view += `<div class="weather__details">
+                <p>The amount of rainfall: ${dataFromApi.current.precip_mm}mm </p>
+                <p>Humidity: ${dataFromApi.current.humidity}%</p>
+                <p>Wind: ${dataFromApi.current.wind_kph}km/h</p>
+            </div>`
+
+        view += `</div>`
+
+        apiDataContainer.innerHTML = view
+    })
+
+    event.preventDefault()
+})
+
