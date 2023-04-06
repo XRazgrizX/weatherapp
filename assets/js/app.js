f32919633e319b23969e0e657aaccb0359f5c2d3
Razgriz <enzerux@gmail.com>
2023-04-06 20:11:10 +0200

    Weather App v2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,46 @@
 let weatherForm = document.querySelector('.weather__form')
 let inputCity = document.querySelector('.weather__city')
-let apiUrl = 'https://api.weatherapi.com/v1/current.json?key=82089a454a5a462da3f131227230104&aqi=yes&q='
-let apiDataContainer = document.querySelector('.weather__data')
+let apiDataConteiner = document.querySelector(".weather__data")
+let loader = document.querySelector(".weather__loader")
 
-weatherForm.addEventListener('submit', (event) => {
-    let city = inputCity.value
+let apiUrl = "https://api.weatherapi.com/v1/current.json?key=ce907bc63f07404485c131208230104&aqi=yes&q="
+
+
+weatherForm.addEventListener("submit", (event) => {
+    showLoader()
+    let city = inputCity.value;
     let fullApiUrl = apiUrl + city
 
-    fetch(fullApiUrl).then(response => response.json()).then((dataFromApi) => {
+    fetch(fullApiUrl).then(response => {
+        hideLoader()
+        if (response.status === 200) {
+            return response.json()
+        }
+
+        throw new Error('API Error')
+
+    }).then((dataFromApi) => {
         // console.log(dataFromApi.current.temp_c)
         let view = ``
-        // view += `W ${dataFromApi.location.name} jest dzisiaj ${dataFromApi.current.temp_c} stopni Celsjusza.`
+
+        // data
+        // view += `W ${dataFromApi.location.name} jest dzisiaj ${dataFromApi.current.temp_c} stopni celciusza.`
 
         view += `<div class="weather__info">`
-
-        view += `<div class="weather__icon">
-        
-        <img src="${dataFromApi.current.condition.icon}" alt="${dataFromApi.current.condition.text}">
-        
+        // county, city, time(region)
+        view += `<div class="weather_region"> 
+                </p>${dataFromApi.location.name}</p>
+                </p>${dataFromApi.location.country}</p>
+                <p> ${dataFromApi.location.localtime}</p>
         </div>`
 
-        view += `<div class="weather__temp">
-        
-        ${dataFromApi.current.temp_c}<span>&deg;C</span>
+        //icon
+        view += `<div class="weather__icon"><img src= ${dataFromApi.current.condition.icon} 
+        alt = ${dataFromApi.current.condition.text}></div >`
+        //temp
+        view += `<div class="weather__temp"> ${dataFromApi.current.temp_c} <span>&degC</span></div >`
 
-        </div>`
-
+        //details
         view += `<div class="weather__details">
                 <p>The amount of rainfall: ${dataFromApi.current.precip_mm}mm </p>
                 <p>Humidity: ${dataFromApi.current.humidity}%</p>
@@ -34,9 +49,34 @@ weatherForm.addEventListener('submit', (event) => {
 
         view += `</div>`
 
-        apiDataContainer.innerHTML = view
+        apiDataConteiner.innerHTML = view
+
+    }).catch((error) => {
+        showError()
     })
 
     event.preventDefault()
 })
 
+// Show error function
+
+let showError = () => {
+    apiDataConteiner.innerHTML = `<div class="weather__error">City not found or we have a problem with API</div>`
+}
+
+let showLoader = () => {
+    loader.style.display = 'block'
+}
+
+let hideLoader = () => {
+    loader.style.display = 'none'
+}
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+        navigator.serviceWorker
+            .register("/serviceWorker.js")
+            .then(res => console.log("service worker registered"))
+            .catch(err => console.log("service worker not registered", err))
+    })
+}
